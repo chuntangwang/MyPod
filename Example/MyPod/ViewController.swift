e1522e813a2f5629c69365f05a9683cf93ef11e1
Chun-Tang Wang <digdoritos@gmail.com>
2018-08-07 02:05:12 +0800

    Initial commit

---
+++ Example/MyPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyPod
+//
+//  Created by Chun-Tang Wang on 08/07/2018.
+//  Copyright (c) 2018 Chun-Tang Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
